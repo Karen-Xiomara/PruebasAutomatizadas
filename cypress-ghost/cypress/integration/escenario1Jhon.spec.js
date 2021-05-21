8627cd25e7b42c970efb32a6c11c6a36981abb06
AbelardoCuesta <j.cuestaa@uniandes.edu.co>
2021-05-21 15:18:08 -0500

    Ajustes en el archivo con la estrategia seudo aleatorio

--- cypress-ghost/cypress/integration/escenario1Jhon.spec.js
+++ cypress-ghost/cypress/integration/escenario1Jhon.spec.js
@@ -1,30 +1,28 @@
 import Seudo from "../Mocks/dataSeudo.json";
 const faker = require('faker');
 
-describe("Create a page Schedule", () => {
+describe('Modify the title and content of a post', () => {
   let titles = Seudo;
   for (let rnd in titles) {
-    it('Modify a post', () => { 
-      //cy.writeFile('cypress/Mocks/dataSeudo.json', { name: 'Eliza', email: 'eliza@example.com' })
-      cy.visit('http://localhost:2369/ghost/#/signin')
-      cy.get('.email.ember-text-field.gh-input.ember-view')
-      .type('j.cuestaa@uniandes.edu.co')
-      cy.get('.password.ember-text-field.gh-input.ember-view')
-      .type('hqlDIOPN9j')
-      cy.get('.login.gh-btn.gh-btn-blue.gh-btn-block.gh-btn-icon.ember-view').click()
-      cy.wait(1000)  
-      cy.get('[id$=ember28]').click()
-      cy.wait(5000)
-      cy.get('h3').contains('Creating').click()
-      cy.wait(5000)
-      cy.get('.gh-editor-title.ember-text-area.gh-input.ember-view').type(titles[rnd].name)
-      cy.get('.gh-publishmenu.ember-view').click()
-      cy.get('.gh-publishmenu-radio.active').click()
-      cy.get('.gh-btn.gh-btn-blue.gh-publishmenu-button.gh-btn-icon.ember-view').click()
-
+  it('Modify a post with numbers in the title and description', () => {
+    cy.visit('http://localhost:2369/ghost/#/signin')
+    cy.get('.email.ember-text-field.gh-input.ember-view')
+    .type('j.cuestaa@uniandes.edu.co')
+    cy.get('.password.ember-text-field.gh-input.ember-view')
+    .type('hqlDIOPN9j')
+    cy.get('.login.gh-btn.gh-btn-blue.gh-btn-block.gh-btn-icon.ember-view').click()
+    cy.wait(1000)
+    cy.get('[id$=ember28]').click()
+    cy.wait(5000)
+    cy.get('h3').contains('Organi').click()
+    cy.wait(5000)
+    cy.get('.gh-editor-title.ember-text-area.gh-input.ember-view').type(titles[rnd].titulo)
+    cy.get('.gh-publishmenu.ember-view').click()
+    cy.get('.gh-publishmenu-radio.active').click()
+    cy.get('.gh-btn.gh-btn-blue.gh-publishmenu-button.gh-btn-icon.ember-view').click()
   })
 }
-});
-beforeEach(() => {
-  cy.writeFile('cypress/Mocks/dataSeudo.json', { name: faker.address.city(), email: 'eliza@example.com' })
 })
+beforeEach(() => {
+  cy.writeFile('cypress/Mocks/dataSeudo.json', [{ titulo: faker.phone.phoneNumber(), descripcion: faker.phone.phoneNumber()},{ titulo: faker.phone.phoneNumber(), descripcion: faker.phone.phoneNumber()},{ titulo: faker.phone.phoneNumber(), descripcion: faker.phone.phoneNumber()}])
+  })
