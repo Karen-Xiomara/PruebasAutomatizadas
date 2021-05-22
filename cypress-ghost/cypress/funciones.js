d8a1d1ff0d747cf01cce785de1e9bd8838fcc0a3
Alberto Chica <a.chica@uniandes.edu.co>
2021-05-22 17:51:03 -0500

    Nueva Funcion para Grabar Registros en Data de Post

--- cypress-ghost/cypress/funciones.js
+++ cypress-ghost/cypress/funciones.js
@@ -11,3 +11,24 @@ export function crearPagina() {
     cy.writeFile(filename, list);
   });
 }
+
+
+export function crearPostAJC() {
+  const filenameA = "cypress/Mocks/MOCK_DATA_AJC.json";
+
+  cy.readFile(filenameA).then((list) => {
+    list.push({
+      id: faker.random.number(100, 1000),
+      Titulo: faker.lorem.sentence(),
+      Detalle: faker.lorem.sentence(),
+      email: 'ghghg@.con',
+      DetalleEsp: faker.lorem.sentence(),
+      ip_adress: faker.lorem.sentence(),
+      Password: faker.lorem.sentence(),
+      FechasFuturas: faker.lorem.sentence(),
+      FechasPasadas: faker.lorem.sentence(),
+      
+    });
+    cy.writeFile(filenameA, list);
+  });
+}
